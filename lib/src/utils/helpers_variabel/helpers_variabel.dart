06fe25612a59276fa3717bb9deb9d2afacd59a43
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-03-31 23:38:40 +0700

    feat: Update code and add some variabel

---
+++ lib/src/utils/helpers_variabel/helpers_variabel.dart
@@ -0,0 +1,22 @@
+class HelpersVariabel {
+  //* Images on boarding screen
+  static const imageOneOnBoarding = "assets/images/on_boarding/image-1.png";
+  static const imageTwoOnBoarding = "assets/images/on_boarding/image-2.png";
+  static const imageThreeOnBoarding = "assets/images/on_boarding/image-3.png";
+
+  //* Title on boarding screen
+  static const titleOneOnBoarding = "Welcome to Expense Tracker!";
+  static const titleTwoOnBoarding = "Take Control of Your Finances!";
+  static const titleThreeOnBoarding = "Start Saving Today!";
+
+  //* Subtitle on boarding screen
+  static const subTitleOneOnBoarding = "Track Your Expenses Effortlessly";
+  static const subTitleTwoOnBoarding = "Efficiently Manage Your Spending";
+  static const subTitleThreeOnBoarding = "Achieve Financial Goals with Ease.";
+
+  //* Time-based welcome greetings on main screen
+  static const welcomeTitleOne = "Good morning!";
+  static const welcomeTitleTwo = "Hello afternoon!";
+  static const welcomeTitleThree = "Good evening!!";
+  static const welcomeTitleFour = "Hi there, night!";
+}
