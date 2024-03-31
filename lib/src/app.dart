ce33115d9523d8a42c390bfc0dc3b56bb7c1c6c6
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-03-31 23:35:17 +0700

    feat: Update code formatting and fix minor bugs

---
+++ lib/src/app.dart
@@ -0,0 +1,24 @@
+import 'package:expense_app/src/features/input_screen/views/input_screen.dart';
+import 'package:expense_app/src/features/main_screen/views/main_screen.dart';
+import 'package:expense_app/src/features/on_boarding/views/on_boarding_scren.dart';
+import 'package:expense_app/src/utils/theme/theme_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: Provider.of<ThemeProvider>(context).themeData,
+      routes: {
+        '/mainscreen': (context) => const MainScreen(),
+        '/onboardingscreen': (context) => const OnBoardingScreen(),
+        '/inputscreen':(context) => const InputScreen(),
+      },
+      home: const OnBoardingScreen(),
+    );
+  }
+}
