e3c53b1579384bfb0a8fd6d9cb7d3d2e4ada9bc2
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-04-02 00:55:53 +0700

    feat: Refactor dispose method and initialize Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:expense_app/firebase_options.dart';
 import 'package:expense_app/src/app.dart';
 import 'package:expense_app/src/utils/theme/theme_provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+Future<void> main() async {
   runApp(
     MultiProvider(
       providers: [
@@ -11,5 +13,8 @@ void main() {
       ],
       child: const App(),
     ),
+  );
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
 }
