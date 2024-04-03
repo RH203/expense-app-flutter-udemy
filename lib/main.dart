32aab4a70f0b857b5c55926968233400a307a1d4
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-04-03 23:43:33 +0700

    feat: Fix theme and authentication screens

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,10 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     MultiProvider(
       providers: [
@@ -13,8 +17,5 @@ Future<void> main() async {
       ],
       child: const App(),
     ),
-  );
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
   );
 }
