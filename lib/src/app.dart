a9d0fa80e14affd02f3a35f371fed756eefa1433
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-04-03 23:44:01 +0700

    feat: Add new screens and update theme

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,3 +1,4 @@
+import 'package:expense_app/src/features/auth_screen/views/check_page.dart';
 import 'package:expense_app/src/features/auth_screen/views/sign_in_screen.dart';
 import 'package:expense_app/src/features/auth_screen/views/sign_up_screen.dart';
 import 'package:expense_app/src/features/input_screen/views/input_screen.dart';
@@ -13,14 +14,20 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "Expense APP",
       debugShowCheckedModeBanner: false,
       theme: Provider.of<ThemeProvider>(context).themeData,
       routes: {
+        //? Main screen
         '/mainscreen': (context) => const MainScreen(),
+        //? Onboarding screen
         '/onboardingscreen': (context) => const OnBoardingScreen(),
+        //? Input screen
         '/inputscreen': (context) => const InputScreen(),
+        //? Auth screen
         '/signinscreen': (context) => const SignInScreen(),
         '/signupscreen': (context) => const SignUpScreen(),
+        '/checkpagescreen': (context) =>  CheckPage(),
       },
       home: const OnBoardingScreen(),
     );
